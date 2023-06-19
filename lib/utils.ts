d1cd6ddcaefc890ed6341f2545bf0129ff4e6534
dervism <dervis4@gmail.com>
2023-06-19 03:24:51 +0200

    Adding a avg function for later use

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,9 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import {type ClassValue, clsx} from "clsx"
+import {twMerge} from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const avg = (numbers: number[]) => numbers && numbers.length > 0 ?
+  numbers.reduce((p, c) => p + c) / numbers.length : -1;
