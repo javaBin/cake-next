f151b42f73bb5c97173deb963bf480f84f904985
dervism <dervis4@gmail.com>
2023-06-25 18:30:59 +0200

    Add better caching and preloading of data

---
+++ app/cake/data/db/sleepingPill.ts
@@ -0,0 +1,60 @@
+import {Conference, SleepingPillAllSessionsResponse, SleepingPillResponse} from "@/types/talk";
+import 'server-only'
+import * as allConferences from "./all.json"
+
+const url = "https://sleepingpill.javazone.no/public/allSessions/";
+const allSessionsUrl = "https://sleepingpill.javazone.no/public/allSessions"
+
+export function getConference(year: number): Conference {
+  const conference = allConferences.conferences.find(c => c.year == year)
+
+  if (!conference) throw new Error("Cannot find conference " + year)
+
+  return conference
+}
+
+export const getPublicJavaZoneData = async (year: string): Promise<SleepingPillResponse> => {
+  const res = await fetch(`${url + year}`)
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export const prefetchAllConferencesSync = async (progressCallback?: (n: number) => void): Promise<SleepingPillResponse[]> => {
+  const allConferencesList: SleepingPillAllSessionsResponse = allConferences;
+
+  const total = allConferencesList.conferences.length
+  const conferences: SleepingPillResponse[] = []
+
+  for (let i = 0; i < total; i++) {
+    conferences.push(
+      await getPublicJavaZoneData(allConferencesList.conferences[i].slug)
+    )
+
+    if (progressCallback) progressCallback(((i + 1) / total) * 100);
+  }
+
+  return Promise.resolve(conferences);
+}
+
+export const prefetchAllConferencesAsync = () => {
+  const allConferencesList: SleepingPillAllSessionsResponse = allConferences;
+  void allConferencesList.conferences.map(value => getPublicJavaZoneData(value.slug))
+}
+
+export const preloadJavaZoneData = (year: string) => {
+  void getPublicJavaZoneData(year)
+}
+
+export const preloadAllConferencesList = async (): Promise<SleepingPillAllSessionsResponse> => {
+  const allSessions = await fetch(allSessionsUrl);
+
+  if (!allSessions.ok) {
+    throw new Error('Failed to preload all conference list')
+  }
+
+  return allSessions.json();
+}
