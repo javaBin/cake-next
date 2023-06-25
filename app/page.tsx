2737c9a592fd2eb7a0d12bfc3a3437ea6f94eb98
dervism <dervis4@gmail.com>
2023-06-25 18:30:05 +0200

    Update app page to handle skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link"
 
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+import {siteConfig} from "@/config/site"
+import {buttonVariants} from "@/components/ui/button"
 
 export default function IndexPage() {
+  //prefetchAllConferencesAsync();
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
