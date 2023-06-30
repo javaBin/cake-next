052023ba9c51d01476079b96eb39782a623ed8a0
dervism <dervis4@gmail.com>
2023-06-30 16:25:59 +0200

    Prefetch data

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import Link from "next/link"
 
 import {siteConfig} from "@/config/site"
 import {buttonVariants} from "@/components/ui/button"
+import {preloadAllConferencesAsync, preloadAllConferencesList} from "@/app/cake/data/db/sleepingPill";
 
 export default function IndexPage() {
-  //prefetchAllConferencesAsync();
+  preloadAllConferencesAsync();
+  preloadAllConferencesList();
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
